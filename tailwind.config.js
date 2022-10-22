5ae8900060d5df0e2b9799dd1a9cf5cfc3e72e65
maxcaplan <maxacaplan@gmail.com>
2022-10-22 07:51:14 -0300

    Added custom text styling for projects

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,43 @@ module.exports = {
                 'sans': ['Manrope', ...defaultTheme.fontFamily.sans],
                 'display': ['JetBrains Mono', ...defaultTheme.fontFamily.sans],
             },
+
+            // tailwind typography theme
+            typography: theme => {
+                // console.log(theme())
+
+                return {
+                    DEFAULT: {
+                        css: {
+                            'h1,h2,h3,h4,h5,h6': {
+                                fontFamily: theme('fontFamily.display').join(", "),
+                                fontWeight: 700,
+                            },
+
+                            pre: {
+                                backgroundColor: theme('colors.neutral.800'),
+                                borderRadius: theme('borderRadius.DEFAULT'),
+                            },
+
+                            code: {
+                                fontFamily: theme('fontFamily.display').join(", "),
+                                backgroundColor: theme('colors.neutral.800'),
+                                padding: `${theme('padding.[0.5]')} ${theme('padding.1')}`,
+                                margin: `0 ${theme('padding.[0.5]')}`,
+                                borderRadius: theme('borderRadius.DEFAULT'),
+                                display: 'inline-block'
+                            },
+
+                            'code::before': {
+                                content: '""',
+                            },
+                            'code::after': {
+                                content: '""',
+                            },
+                        },
+                    },
+                }
+            }
         },
     },
     plugins: [require('@tailwindcss/typography')],
