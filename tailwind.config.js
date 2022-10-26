b2a9792b81e9c1ba3bf368293023539bc0a8c81b
maxcaplan <maxacaplan@gmail.com>
2022-10-26 15:33:07 -0300

    Tidy upped tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,50 +12,49 @@ module.exports = {
     ],
     theme: {
         extend: {
+            // Custom fonts
             fontFamily: {
                 'sans': ['Manrope', ...defaultTheme.fontFamily.sans],
                 'display': ['JetBrains Mono', ...defaultTheme.fontFamily.mono],
             },
 
-            // tailwind typography theme
-            typography: theme => {
-                // console.log(theme())
+            // Tailwind typography theme
+            typography: ({ theme }) => ({
+                DEFAULT: {
+                    css: {
+                        'h1,h2,h3,h4,h5,h6': {
+                            fontFamily: theme('fontFamily.display').join(", "),
+                            fontWeight: 700,
+                        },
 
-                return {
-                    DEFAULT: {
-                        css: {
-                            'h1,h2,h3,h4,h5,h6': {
-                                fontFamily: theme('fontFamily.display').join(", "),
-                                fontWeight: 700,
-                            },
+                        pre: {
+                            backgroundColor: theme('colors.neutral.800'),
+                            borderRadius: theme('borderRadius.DEFAULT'),
+                        },
 
-                            pre: {
-                                backgroundColor: theme('colors.neutral.800'),
-                                borderRadius: theme('borderRadius.DEFAULT'),
-                            },
+                        code: {
+                            fontFamily: theme('fontFamily.display').join(", "),
+                            backgroundColor: theme('colors.neutral.800'),
+                            padding: `${theme('padding.[0.5]')} ${theme('padding.1')}`,
+                            margin: `0 ${theme('padding.[0.5]')}`,
+                            borderRadius: theme('borderRadius.DEFAULT'),
+                            display: 'inline-block'
+                        },
 
-                            code: {
-                                fontFamily: theme('fontFamily.display').join(", "),
-                                backgroundColor: theme('colors.neutral.800'),
-                                padding: `${theme('padding.[0.5]')} ${theme('padding.1')}`,
-                                margin: `0 ${theme('padding.[0.5]')}`,
-                                borderRadius: theme('borderRadius.DEFAULT'),
-                                display: 'inline-block'
-                            },
-
-                            'code::before': {
-                                content: '""',
-                            },
-                            'code::after': {
-                                content: '""',
-                            },
+                        'code::before': {
+                            content: '""',
+                        },
+                        'code::after': {
+                            content: '""',
                         },
                     },
-                }
-            }
+                },
+            })
         },
     },
+
     plugins: [require('@tailwindcss/typography')],
+
     // Disables hover styling on mobile devices
     future: {
         hoverOnlyWhenSupported: true,
