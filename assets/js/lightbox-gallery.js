cdc2091951bfd286f8931420a928d6881026ddd0
maxcaplan <maxacaplan@gmail.com>
2022-10-27 15:04:28 -0300

    Added lazy video loading for gallery component

--- assets/js/lightbox-gallery.js
+++ assets/js/lightbox-gallery.js
@@ -167,3 +167,40 @@ function closeLightbox(lightbox) {
     lightbox.classList.add("hidden")
     document.documentElement.style.overflowY = "auto"
 }
+
+/** 
+ * Lazy loads autoplay video content 
+ * Implementation adapted from https://web.dev/lazy-loading-video/#video-gif-replacement
+ */
+document.addEventListener("DOMContentLoaded", function () {
+    // Get all videos to be lazy loaded
+    var lazyVideos = Array.from(document.querySelectorAll("video.lazy-load"))
+
+    if ("IntersectionObserver" in window) {
+        // Create intersection observer
+        var lazyVideoObserver = new IntersectionObserver(entries => {
+            entries.forEach(video => {
+                // Check if video is visible
+                if (video.isIntersecting) {
+                    // Enable video sources to load
+                    for (var source in video.target.children) {
+                        var videoSource = video.target.children[source];
+                        if (typeof videoSource.tagName === "string" && videoSource.tagName === "SOURCE") {
+                            videoSource.src = videoSource.dataset.src;
+                        }
+                    }
+
+                    // Load video
+                    video.target.load();
+                    video.target.classList.remove("lazy-load");
+                    lazyVideoObserver.unobserve(video.target);
+                }
+            });
+        });
+
+        // Observe all videos to lazy load
+        lazyVideos.forEach(lazyVideo => {
+            lazyVideoObserver.observe(lazyVideo);
+        });
+    }
+});
